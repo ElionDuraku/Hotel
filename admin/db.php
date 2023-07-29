c45fe2ba40c8f2b9eb5562a79425d6cf4ee46e18
Elion Duraku <elionduraku11@gmail.com>
2023-07-29 13:38:12 +0200

    first commit

---
+++ admin/db.php
@@ -0,0 +1,10 @@
+<?php
+$con = mysqli_connect("localhost","root","","sourcecodester_hoteldb") or die(mysql_error());
+
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "sourcecodester_hoteldb");
+
+
+?>
