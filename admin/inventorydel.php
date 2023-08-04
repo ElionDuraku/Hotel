ea41c74dd93d69c131f32205892b9cf6d5a53759
Elion Duraku <elionduraku11@gmail.com>
2023-08-04 14:48:33 +0200

    Inventory delete

--- admin/inventorydel.php
+++ admin/inventorydel.php
@@ -5,14 +5,14 @@ include ('db.php');
 
 			
 			$id =$_GET['eid'];		
-			$newsql ="DELETE FROM `login` WHERE id ='$id' ";
+			$newsql ="DELETE FROM `inventory` WHERE id ='$id' ";
 			if(mysqli_query($con,$newsql))
 				{
 				echo' <script language="javascript" type="text/javascript"> alert("User name and password Added") </script>';
 							
 						
 				}
-			header("Location: usersetting.php");
+			header("Location: inventory.php");
 		
 ?>
 
